60ec54da4e5b67f4bebcc48976a303bad65d0d22
nyrobert <nyitrai.robert@gmail.com>
2016-08-11 21:49:33 +0000

    update content negotiation tests

--- tests/api/ContentNegotiationCest.php
+++ tests/api/ContentNegotiationCest.php
@@ -1,14 +1,19 @@
 <?php
 
+use Api\App;
+use Api\Exception\UnsupportedMediaTypeParameter;
 use Codeception\Util\HttpCode;
 
+/**
+ * @link http://jsonapi.org/format/#content-negotiation
+ */
 class ContentNegotiationCest
 {
 	const AUTHOR_ID = '11a38b9a-b3da-360f-9353-a5a725514269';
 
 	public function validAcceptHeader(ApiTester $I)
 	{
-		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
+		$I->haveHttpHeader('Accept', App::MEDIA_TYPE);
 		$I->sendGET('/authors/'.self::AUTHOR_ID);
 		$I->seeResponseCodeIs(HttpCode::OK);
 		$I->seeResponseIsJson();
@@ -16,11 +21,13 @@ class ContentNegotiationCest
 
 	public function invalidAcceptHeader(ApiTester $I)
 	{
-		$I->haveHttpHeader('Accept', 'application/vnd.api+json;param=1');
+		$I->haveHttpHeader('Accept', App::MEDIA_TYPE.';param=1');
 		$I->sendGET('/authors/'.self::AUTHOR_ID);
 		$I->seeResponseCodeIs(HttpCode::NOT_ACCEPTABLE);
 		$I->seeResponseIsJson();
-		$I->seeResponseContainsJson(['code'   => 'UnsupportedMediaTypeParameter']);
-		$I->seeResponseContainsJson(['status' => (string) HttpCode::NOT_ACCEPTABLE]);
+		$I->seeResponseContainsJson(['errors' => [
+			'code'   => (new UnsupportedMediaTypeParameter())->getName(),
+			'status' => (string) HttpCode::NOT_ACCEPTABLE,
+		]]);
 	}
 }
